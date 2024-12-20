dbccd6ecf64248ebbfa753b4092ec689d6c2caeb
LaybeR <46791108+LaybeRize@users.noreply.github.com>
2024-12-20 19:45:10 +0100

    fixed styling and generation of footnotes

--- handler/helper.go
+++ handler/helper.go
@@ -13,11 +13,12 @@ var extensions = parser.NewWithExtensions(parser.NoIntraEmphasis | parser.Tables
 	parser.Autolink | parser.Strikethrough | parser.SpaceHeadings | parser.OrderedListStart |
 	parser.BackslashLineBreak | parser.DefinitionLists | parser.EmptyLinesBreakList | parser.Footnotes |
 	parser.SuperSubscript)
+var policy = bluemonday.UGCPolicy().AllowAttrs("class").OnElements("div")
 
 func MakeMarkdown(md string) template.HTML {
 	intermediate := markdown.NormalizeNewlines([]byte(md))
 	maybeUnsafeHTML := markdown.ToHTML(intermediate, extensions, nil)
-	htmlResult := string(bluemonday.UGCPolicy().SanitizeBytes(maybeUnsafeHTML))
+	htmlResult := string(policy.SanitizeBytes(maybeUnsafeHTML))
 	return template.HTML(strings.ReplaceAll(strings.ReplaceAll(htmlResult, "<code>\n", "<code>"), "\n</code>", "</code>"))
 }
 
